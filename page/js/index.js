d5b4a9a17e23c0155e2dfd81c229b976aa6e3a7f
zhoulei0232 <5971794@qq.com>
2020-06-15 17:45:07 +0800

    0615提交

---
+++ page/js/index.js
@@ -0,0 +1,13 @@
+var everyDay = new Vue({
+    el:"#every_day",
+    data:{
+        content:"ddddaa"
+    },
+    computed:{
+        getContent:function(){
+            return  this.content;
+        }
+    },
+    created:function(){}
+
+});
